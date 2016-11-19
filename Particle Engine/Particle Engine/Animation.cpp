11775a5d5b3e2f009ff15abd088ea2f6c214a168
oglofch <oglfcheski@hotmail.com>
2016-11-19 14:33:17 -0800

    Remove stuff

--- Particle Engine/Particle Engine/Animation.cpp
+++
@@ -1,9 +0,0 @@
-#include "Animation.h"
-
-Animation::Animation() {
-	m_isFinished = false;
-}
-
-bool Animation::isFinished() {
-	return m_isFinished;
-}
--- Particle Engine/Particle Engine/Constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CONSTANTS_H_
-#define _CONSTANTS_H_
-
-#define PPM 30.0f
-#define PI	3.14159
-#define GRAVITY 30.0f
-
-#endif
